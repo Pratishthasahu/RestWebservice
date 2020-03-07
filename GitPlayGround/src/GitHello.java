5c9c0ba4cc4985897ec50c6a11d48388543b4d90
Training <Training>
2020-03-07 14:48:18 +0530

    second

--- GitPlayGround/src/GitHello.java
+++ GitPlayGround/src/GitHello.java
@@ -4,6 +4,7 @@ public class GitHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello!!My first Git");
+		System.out.println("Second");
 	}
 
 }
