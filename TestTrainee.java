29cf5e41ade00e0383a2708f5b4273ce43e72923
Pratishthasahu <46573672+Pratishthasahu@users.noreply.github.com>
2020-03-11 12:33:59 +0530

    Add files via upload

---
+++ TestTrainee.java
@@ -0,0 +1,11 @@
+import lti.*;
+
+
+public class TestTrainee {
+	public static void main(String[] args) {
+		Trainee t1 = new Trainee();
+		Emp e1 = new Emp();
+		t1.display();
+		e1.display();
+	}
+}
